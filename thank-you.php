e3cbb196dfda89882869079f6bd50253409e9dbd
eugenerepetsky <evgeniyrepetsky@gmail.com>
2020-09-23 20:01:50 +0300

    Create thank-you.php

---
+++ thank-you.php
@@ -0,0 +1,159 @@
+<?php
+	require('head.php');
+?>
+	<title>
+		<?php
+			switch ($lang) {
+				case 'ua':
+					echo "РСМ Україна - Дякуємо за пропозицію";
+					break;
+				case 'en':
+					echo "RSM Ukraine - Thanks for your offer";
+					break;
+			}
+		?>
+	</title>
+	<style>
+
+		.thank-you__content {
+			padding: 150px 10px;
+			text-align: center;
+		}
+		.bird-container {
+			display: flex;
+			justify-content: center;
+			height: 90px;
+			align-items: center;
+		}
+		.bird__circle {
+			position: relative;
+			width: 90px;
+			height: 90px;
+			background: rgba(255,255,255,1);
+			border-radius: 50%;
+			border: 1px solid rgba(63,156,53,1);
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			transition: 0.5s;
+			padding-bottom: 10px;
+		}
+		.bird__circle:hover {
+			background-color: rgba(63,156,53,1);
+			cursor: pointer;
+			width: 240px;
+			border-radius: 50px;
+			padding-bottom: 0px;
+			padding-right: 130px;
+			height: 60px;
+		}
+		.bird-container:hover .bird__circle {
+			background-color: rgba(63,156,53,1);
+			cursor: pointer;
+			width: 240px;
+			border-radius: 50px;
+			padding-bottom: 0px;
+			padding-right: 140px;
+			height: 60px;
+		}
+		.back-to-main {
+			position: absolute;
+			color:rgba(255,255,255,1);
+			height: 0;
+			width: 150px;
+			transform: scaleX(0);
+			display: flex;
+			align-items: center;
+			right: 0;
+			overflow: hidden;
+			transition: 0.5s;
+			font-size: 0;
+		}
+		.bird-container:hover .back-to-main{
+			height: 100%;
+			right: 10px;
+			font-size: 18px;
+			transform: scaleX(1);
+		}
+		.bird {
+			height: 20px;
+			width: 40px;
+			border: 6px solid rgba(63,156,53,1);
+			border-right: none;
+			border-top: none;
+			transform: rotate(-45deg);
+			transition: 0.5s;
+			position: relative;
+		}
+		.bird::before {
+			position: absolute;
+			content: '';
+			display: block;
+			height: 12px;
+			width: 12px;
+			left: -3px;
+			top: -6px;
+			border: 0px solid rgba(255,255,255,0);
+			border-right: none;
+			border-top: none;
+			transform: rotate(45deg);
+			transition: 0.5s;
+		}
+		.bird-container:hover .bird {
+			border-color: rgba(255,255,255,1);
+			height: 0;
+			width: 25px;
+			border-width: 4px;
+			transform: rotate(0deg);
+		}
+		.bird-container:hover .bird::before {
+			border: 4px solid rgba(255,255,255,1);
+			border-right: none;
+			border-top: none;
+		}
+		.thank-you__title {
+			font-size: 60px;
+			color: rgba(63,156,53);
+			padding: 20px;
+		}
+		.thank-you__sub-title {
+			font-size: 20px;
+			padding: 30px 20px;
+			color: rgba(99,102,106);
+			line-height: 30px;
+		}
+	</style>
+	<?php require('header_new.php');?>
+	<div class="thank-you__content-container">
+		<div class="container">
+			<div class="thank-you__content">
+				<div class="thank-you__title">
+					<?php
+						switch ($lang) {
+							case 'ua':
+								echo "Дякуємо!";
+								break;
+							case 'en':
+								echo "Thank you!";
+								break;
+						}
+					?>
+				</div>
+				<div class="bird-container">
+					<div class="bird__circle">
+						<div class="back-to-main">
+							To main page
+						</div>
+						<div class="bird">
+							
+						</div>
+					</div>
+				</div>
+				<div class="thank-you__sub-title">
+					<a href = "/<?php echo $lang?>">Thank you for the offer. We will contact you as soon as possible.</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+<?php require('footer_new.php');?>
